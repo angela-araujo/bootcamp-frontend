1e59d15d9f6dd75cd417b8df7f55590e72fde58c
angela-araujo <angela.araujo.rj@gmail.com>
2021-12-21 21:21:10 -0300

    adicionando itens inputs e esilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Login from './screens/login/login.screen';
-import {Route, Routes} from 'react-router-dom';
-import {LoginPath} from './screens/login/login.types';
+import { Route, Routes } from 'react-router-dom';
+import { LoginPath } from './screens/login/login.types';
 import { ThemeProvider } from 'styled-components';
 import theme from './themes/main/theme';
+import { GlobalStyle } from './themes/main/global-style';
 
 
 function App() {
   return (
-      <ThemeProvider theme={theme}>      
-          <Routes>
-              <Route path={LoginPath} element={<Login />} />
-          </Routes>
-      </ThemeProvider>
+    <>
+        <ThemeProvider theme={theme}>      
+            <GlobalStyle />
+            <Routes>
+                <Route path={LoginPath} element={<Login />} />
+            </Routes>
+        </ThemeProvider>
+    </>
   );
 }
 
--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import Button from '../../components/buttons/button/button.component';
 import {Container, Grid} from '@mui/material';
+import InputText from '../../components/inputs/input-text.components';
+import login from '../../assets/icons/logo.svg';
+import { Logo, Wrapper } from './login.styled';
 
 export default function Login() {
     return (
         <>
             <Container>
-                <Grid container justifyContent={'center'}>
-                    <Grid item xs={4}>
-                        <Button primary>Entrar</Button>
+                <Wrapper container justifyContent={'center'}>
+                    <Grid item xs={3} justifyContent={'center'}>
+                        <Grid container justifyContent={'center'}>
+                            <Logo src={login} alt={'Netflix logo'} />
+                            <InputText type={'text'} placeholder={'E-mail'} />
+                            <InputText type={'password'} placeholder={'Senha'} />
+                            <Button primary>Entrar</Button>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Wrapper>
             </Container>
         </>
     )
