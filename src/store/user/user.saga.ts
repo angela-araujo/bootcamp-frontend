86840858e237b9da5417aef59e252289e9b69f15
angela-araujo <angela.araujo.rj@gmail.com>
2022-01-06 20:56:55 -0300

    Adicionado loginByToken no user.saga.ts

--- src/store/user/user.saga.ts
+++ src/store/user/user.saga.ts
@@ -2,7 +2,7 @@ import { put, takeEvery, all, call } from 'redux-saga/effects'
 import {userActions} from "./user.slice";
 import sessionService from "../../services/session/session.service";
 import {AccessTokenStorageKey, UserAction} from "./user.types";
-import { PostSessionNew} from "../../services/user/user.types";
+import { GetSession, PostSessionNew} from "../../services/user/user.types";
 
 export function* login(props: UserAction) {
     try {
@@ -21,12 +21,28 @@ export function* login(props: UserAction) {
     }
 }
 
+// TODO: Melhorar rotina de forma que se o usuário clicar muitas vezes não executar 
 function* watchLogin() {
     yield takeEvery('user/login', login)
 }
 
+export function* loginByToken() {
+    try {
+        const accessToken = localStorage.getItem(AccessTokenStorageKey)
+
+        if (accessToken) {
+            const { data: { userId: id } }: GetSession = yield call(sessionService().getSession, accessToken)
+
+            yield put(userActions.setData({ id }))
+        }
+    } catch (error) {
+        // @ts-ignore
+        yield put(userActions.setError(error.response.data.message))
+    }
+}
 export default function* userSaga() {
     yield all([
         watchLogin(),
+        loginByToken(),
     ])
 }
