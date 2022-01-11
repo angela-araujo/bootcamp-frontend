95618464a3601ae8e52c741ddfc3e893213eb3d4
angela-araujo <angela.araujo.rj@gmail.com>
2022-01-11 20:54:53 -0300

    Alternando url do backend para acessar aws

--- src/modules/axios/axios.module.ts
+++ src/modules/axios/axios.module.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://eixmnainm4.execute-api.us-east-1.amazonaws.com/dev/',
 });
 
 export default axiosInstance;
